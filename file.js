7f6fa4a31ff1be3cbd1304a3d88d8cb6cd7942ad
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2023-03-13 16:06:18 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,76 +1,59 @@
-const input = document.querySelector('#input')
-const main = document.querySelector('main')
-const goBtn = document.querySelector('#goBtn')
-const Api_key = '53015ecfbd59c9ebf385bd744acd3563'
+const temp = document.querySelector('.temp')
+const cityName = document.querySelector('.cityName')
+const des = document.querySelector('.description')
+const feels = document.querySelector('.feels')
+const min = document.querySelector('.min')
+const max = document.querySelector('.max')
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const img = document.querySelector('img')
+const hum = document.querySelector('.hum')
+const press = document.querySelector('.press')
 
-goBtn.addEventListener('click',(e)=>{
-    e.preventDefault()
-    console.log(input.value)
-    getData(input.value)
+window.addEventListener('DOMContentLoaded',()=>{
+    init('jaipur')
 })
-const refresh = ()=>{
-    window.location.reload()
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    init(input.value)
+    input.value=''
+})
+function init (city){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=53015ecfbd59c9ebf385bd744acd3563`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(res=>{
+        if(res.cod == "404"){
+            input.value = 'City not found'
+            setTimeout(() => {
+                input.value=''
+            }, 3000);
+        }else{
+            document.querySelector('.box').style.display = 'block'
+        // console.log(res)
+        updateIcon(res)
+        cityName.innerText = `${res.name}`
+        city.innerText = `${res.name}`
+        temp.innerText = `${res.main.temp} ℃`
+        des.innerText =`${res.weather[0].main}`
+        feels.innerText =`${res.main.feels_like} ℃`
+        min.innerText =`${res.main.temp_min} ℃`
+        max.innerText =`${res.main.temp_max} ℃`
+        hum.innerText =`${res.main.humidity} %`
+        press.innerText =`${res.main.pressure} hPa`
+        }
+    })
+    .catch(err=>console.error(err))
 }
-const showData = (data)=>{
-    const div = document.createElement('div')
-    if(data.cod === '404'){
-        div.innerHTML = `<h2> City not found</h2>
-        <button class='deleteBtn refresh' onClick='refresh()'>Try Again</button>
-        `
-        div.setAttribute('class','notFound')
-        main.append(div)
-        return;
+function updateIcon (data){
+    if(data.weather[0].main === 'Haze'){
+        img.src = `https://www.svgrepo.com/show/344670/cloud-haze-1.svg`
+    }else if(data.weather[0].main === 'Clear'){
+        img.src = `https://www.svgrepo.com/show/493527/sun-flower.svg`
+    }else if(data.weather[0].main === 'Clouds'){
+        img.src = `https://www.svgrepo.com/show/499935/sun-rain.svg`
+    }else{
+        img.src = `https://www.svgrepo.com/show/499943/rain.svg`
     }
-   
-    div.innerHTML = `
-    <div class="box">
-        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-        <p>${data.weather[0].main}</p>
-    </div>
-    <div class="box">
-        <div class='box-sm'>
-        <p>City</p>
-        <span class='capital'>${input.value}</span>
-        </div>
-        <div class='box-sm'>
-        <p>Temprature</p>
-        <span>${data.main.temp} ℃</span>
-        </div>
-    </div>
-    
-    <div class="box">
-    <div class='box-sm'>
-        <p>Humidity</p>
-        <span>${data.main.humidity} %<span/>
-    </div>
-    <div class='box-sm'>
-        <p>Air Pressure</p>
-        <span>${data.main.pressure} hPa</span>
-    </div>
-        
-    </div>
-    <div class="box">
-    <div class='box-sm'>
-        <p>Wind speed</p>
-        <span>${data.wind.speed} kmph</span>
-    </div>
-    <div class='box-sm'>
-        <button class='deleteBtn' onClick='deleteItem()'>Delete</button>
-    </div>
-    </div>
-`
-div.setAttribute('class','weather')
-    main.append(div)
-    input.value = ''
 }
-const deleteItem = (e)=>{
-    const div = document.querySelector('div')
-    div.remove()
-}
-const getData = async (city)=>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}&units=metric`
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-    showData(data)
-}
+
